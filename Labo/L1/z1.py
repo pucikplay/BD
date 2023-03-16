db974f0412b33d782df4c485e1c7d678361c894a
Gabriel <pucikplay@gmail.com>
2023-03-16 17:55:48 +0100

    Second task

--- Labo/L1/z1.py
+++ Labo/L1/z1.py
@@ -1,38 +1,45 @@
 import re
 
-# open files
-text_file = open("Labo/L1/Szekspir/hamlet.txt", mode="r", encoding="utf8")
-stop_words_file = open("Labo/L1/Szekspir/stop_words_english.txt", mode="r", encoding="utf8")
+def importStopWords():
+    stop_words_file = open("Labo/L1/Szekspir/stop_words_english.txt", mode="r", encoding="utf8")
+    stop_words = []
+    for word in stop_words_file:
+        stop_words.append(word[:-1])
+    stop_words_file.close()
+    return stop_words
 
-# import stop words
-stop_words = []
-for word in stop_words_file:
-    stop_words.append(word[:-1])
+def cleanText(path, stop_words):
+    text_file = open(path, mode="r", encoding="utf8")
+    raw_text = text_file.read()
+    text_file.close()
+    lower = raw_text.lower()
+    clean = re.sub("[^a-z0-9]+", " ", lower)
+    T = clean.split(" ")
+    T1 = []
+    for word in T:
+        if not (len(word) < 3 or word in stop_words):
+            T1.append(word)
 
-# clean the text
-raw = text_file.read()
-lower = raw.lower()
-clean = re.sub("[^a-z0-9]+", " ", lower)
-T = clean.split(" ")
-T1 = []
-for word in T:
-    if not (len(word) < 3 or word in stop_words):
-        T1.append(word)
+    dict = {}
+    for word in T1:
+        if word not in dict:
+            dict[word] = 1
+        else:
+            dict[word] += 1
 
-dict = {}
-for word in T1:
-    if word not in dict:
-        dict[word] = 1
-    else:
-        dict[word] += 1
+    return dict
 
-words = sorted(dict.items(), key=lambda x:x[1], reverse=True)
+def sortByFrequency(dict):
+    words = sorted(dict.items(), key=lambda x:x[1], reverse=True)
+    return words
 
-output_file = open("Labo/L1/output.csv", "w")
-output_file.write("\"weight\";\"word\"\n")
-for word in words:
-    output_file.write("\"{}\";\"{}\"\n".format(word[1], word[0]))
+if __name__ == "__main__":
+    stop_words = importStopWords()
+    words = sortByFrequency(cleanText("Labo/L1/Szekspir/hamlet.txt", stop_words))
 
-text_file.close()
-stop_words_file.close()
-output_file.close()
+    output_file = open("Labo/L1/output.csv", "w")
+    output_file.write("\"weight\";\"word\"\n")
+    for word in words:
+        output_file.write("\"{}\";\"{}\"\n".format(word[1], word[0]))
+
+    output_file.close()
---
+++ Labo/L1/z2.py
@@ -0,0 +1,29 @@
+import z1
+import re
+import math
+
+dramas = ["hamlet", "KingLear", "Othello", "RomeoJuliet"]
+n = 4
+
+if __name__ == "__main__":
+    stop_words = z1.importStopWords()
+    documents = {}
+    for drama in dramas:
+        documents[drama] = z1.cleanText("Labo/L1/Szekspir/{}.txt".format(drama), stop_words)
+
+    for drama in dramas:
+        TFIDF = {}
+        for word in documents[drama]:
+            k = 0
+            for _drama in dramas:
+                if word in documents[_drama]:
+                    k += 1
+            IDF = math.log2(n/k)
+            TFIDF[word] = int(documents[drama][word] * IDF)
+
+        output_file = open("Labo/L1/{}_out.csv".format(drama), "w")
+        output_file.write("\"weight\";\"word\"\n")
+        for word in documents[drama]:
+            output_file.write("\"{}\";\"{}\"\n".format(TFIDF[word], word))
+
+        output_file.close()
