b144e7069ed20f1d7653104028295c9c7c67ba5e
Gabriel Budziński <pucikplay@gmail.com>
2023-06-18 14:06:18 +0200

    Task 49 done

--- z49.py
+++ z49.py
@@ -1,6 +1,8 @@
 import random
 from skspatial.objects import Vector,Plane,Point
 import matplotlib.pyplot as plt
+import numpy as np
+import math
 
 def randVect():
     x = random.uniform(-1.0, 1.0)
@@ -14,7 +16,10 @@ def ortonormalBase():
     n2 = randVect()
     n3 = n1.cross(n2).unit()
     n2 = n1.cross(n3).unit()
-    return [n1, n2, n3]
+    # return [n1, n2, n3]
+    return [Vector.from_points([0,0,0],[-0.42223698, 0.90647432, 0.00449915]),
+            Vector.from_points([0,0,0],[-0.86421138, -0.40104291, -0.30381456]),
+            Vector.from_points([0,0,0],[ 0.27359574, 0.13216996, -0.95272056])]
 
 def matrixL():
     L = []
@@ -24,16 +29,33 @@ def matrixL():
                 L.append(Point([x,y,z]))
     return L
 
+def matrixA():
+    A = np.random.rand(2,3)
+    # return A * (1/math.sqrt(2))
+    return np.array([[0.00431902, 0.0879098,  0.44337079],
+    [0.35332662, 0.15498393, 0.4102104]])
 
 if __name__ == "__main__":
     base = ortonormalBase()
-    print(base)
+    for vect in base:
+        print(vect)
     plane = Plane.from_vectors([0,0,0],base[0],base[1])
     L = matrixL()
-    L_proj = []
     _, ax = plt.subplots()
     for point in L:
         point_proj = plane.project_point(point)
         point_proj.plot_2d(ax, s=50)
+    plt.savefig("z49_plane.png", dpi=300)
+    plt.close()
 
-    plt.show()
+    A = matrixA()
+    for row in A:
+        print(row)
+    L_proj = []
+    _, ax = plt.subplots()
+    for point in L:
+        point_proj = Point(A.dot(point))
+        print
+        point_proj.plot_2d(ax, s=50)
+    plt.savefig("z49_A.png", dpi=300)
+    plt.close()
